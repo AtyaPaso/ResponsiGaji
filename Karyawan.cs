4c35ca88b8d70592f956b3770da8b30854e2dbb7
AtyaPaso <atyapaso@students.amikom.ac.id>
2021-04-13 15:25:23 +0700

    Tugas responsi

---
+++ Karyawan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3404
+{
+    class Karyawan
+    {
+        public string nik, nama;
+        public double gaji;
+        public Karyawan (string NIK, string NAMA, double GajiKaryw)
+        {
+            nik = NIK;
+            nama = NAMA;
+            if (GajiKaryw <= 0)
+            {
+                gaji = 0;
+            }
+            else
+            {
+                gaji = GajiKaryw;
+            }
+        }
+        public void NaikanGaji()
+        {
+            gaji = gaji + (gaji * (10.0 / 100.0));
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ResponsiPemrograman3404
+{
+    class Program
+    {
+        static string formatmatauang(int angka)
+        {
+            return String.Format(CultureInfo.CreateSpecificCulture("id-id"), "Rp. {0:N}", angka);
+        }
+
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("20113404", "Atya", 1000000000);
+            Karyawan karyawan2 = new Karyawan("12345677", "ChaEunWoo", 400000000);
+
+            Console.WriteLine("\nNO Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine(" {0} {1}                 {2}", karyawan1.nik, karyawan1.nama, formatmatauang((int)karyawan1.gaji));
+            Console.WriteLine(" {0} {1}                 {2}", karyawan2.nik, karyawan2.nama, formatmatauang((int)karyawan2.gaji));
+
+            karyawan1.NaikanGaji();
+            karyawan2.NaikanGaji();
+
+
+            Console.WriteLine("\nAlhamdulilah naik gaji 10%");
+
+            Console.WriteLine("\nNO Nik/Nama            Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine(" {0} {1}                  {2}", karyawan1.nik, karyawan1.nama, formatmatauang((int)karyawan1.gaji));
+            Console.WriteLine(" {0} {1}                  {2}", karyawan2.nik, karyawan2.nama, formatmatauang((int)karyawan2.gaji));
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
+    
